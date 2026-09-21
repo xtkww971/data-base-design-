e8e450848945235e1558de1e9eabb47ab57314a9
hyun0510 <dbgus0510@naver.com>
2026-09-22 05:45:01 +0900

    mod(query_generator.py) 프롬프트 수정 및 변수 매핑 방식 변경

--- nl2sql/src/nodes/query_generator.py
+++ nl2sql/src/nodes/query_generator.py
@@ -4,7 +4,8 @@ from src.chatmodel import get_llm
 from pydantic import Field, BaseModel
 
 class SQLGenerationResult(BaseModel):
-    sql: str = Field(description="생성된 SQL 쿼리. 마크다운 코드블록 없이 순수 SQL 텍스트만")
+    sql: str = Field(default="", description="생성된 SQL 쿼리. 마크다운 코드블록 없이 순수 SQL 텍스트만")
+    error: str = Field(default="", description="LLM이 판단하기에 정보가 부족하거나 생성 불가할 때 에러 사유 기록")
 
 
 SYSTEM_PROMPT = """당신은 숙력된 SQL 엔지니어 입니다. 주어진 스키마 정보와 예시 쿼리문을 
@@ -15,7 +16,19 @@ SYSTEM_PROMPT = """당신은 숙력된 SQL 엔지니어 입니다. 주어진 스
 2. 스키마에 없는 테이블/컬럼명을 지어내지 마세요.
 3. 결과행이 많더라도 LIMIT은 사용하지 마세요.
 4. 출력은 반드시 json 형태로 출력하세요
-5. 만약 테이블 이름이나 컬럼명(스키마) 정보가 부족하여 SQL을 생성할 수 없다면 'sql' 필드에 "스키마 정보 부족"문자열을 담으세요"
+5. 만약 스키마 정보가 부족하더라도 예시 쿼리문을 참고하여 쿼리를 생성하세요.
+6. sql을 생성하지 못했을 때는 그 이유를 error에 기록하세요.
+
+"""
+
+HUMAN_PROMPT = """DB 스키마 정보:
+{schema_info}
+
+예시 쿼리문:
+{few_shot_examples}
+
+질문:
+{question}
 """
 
 RETRY_PROMPT = """이전에 생성한 쿼리에 문제가 발생하였습니다. 오류를 참고해서 수정하세요
@@ -44,25 +57,29 @@ def query_generator(state: GraphState):
 
     message = [
         ("system", SYSTEM_PROMPT),
-        ("human", f"DB스키마 정보:\n{schema}\n\n예시 쿼리문:\n{example}\n\n질문:\n{question}")
+        ("human", HUMAN_PROMPT)
     ]
-    if state["retry_count"] > 0:
-        message.append(("human", RETRY_PROMPT.format(
-            previous_query = query,
-            error_msg = error
-        )))
 
-    chain = ChatPromptTemplate.from_messages(message) | get_llm().with_structured_output(SQLGenerationResult, method="json_mode")
-
-    result : SQLGenerationResult = chain.invoke({
+    input_variables = {
         "schema_info": schema,
         "few_shot_examples": example,
         "question": question
-    })
+    }
 
+    if state["retry_count"] > 0:
+        message.append(("human", RETRY_PROMPT))
+        input_variables.update({
+            "previous_query": query,
+            "error_msg": error
+        })
+
+    chain = ChatPromptTemplate.from_messages(message) | get_llm().with_structured_output(SQLGenerationResult, method="json_mode")
+
+    result : SQLGenerationResult = chain.invoke(input_variables)
+    
     return {
         "query" : result.sql,
         "retry_count" : state["retry_count"] + 1,
-        "error": ""
+        "error": result.error
     }
 
